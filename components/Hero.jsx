30cf9bf02beae1745fa40211fa6d751ee6419925
IndieHippie <you@example.com>
2024-02-15 13:51:28 +0200

    commit

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -8,7 +8,7 @@ const Hero = ({ heading, message }) => {
   };
 
   return (
-    <div className="flex items-center justify-center h-screen bg-fixed bg-center bg-cover custom-img">
+    <div className="flex items-center justify-center h-screen h-dvh !important bg-fixed bg-center bg-cover custom-img">
       {/* Overlay */}
       <div className="absolute top-0 left-0 right-0 bottom-0 bg-black/70 z-[2]" />
       <div className="p-5 text-white z-[2] mt-[-10rem]">
