45bac7880a8470ec85e306ce1bc8e034b0e1fa7d
IndieHippie <you@example.com>
2024-02-15 14:12:59 +0200

    commit

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -12,8 +12,11 @@ const Hero = ({ heading, message }) => {
       {/* Overlay */}
       <div className="absolute top-0 left-0 right-0 bottom-0 bg-black/70 z-[2]" />
       <div className="p-5 text-white z-[2] mt-[-10rem]">
-        <h2 className="text-5xl font-bold">{heading}</h2>
-        <p className="py-5 text-xl">{message}</p>
+        <div>
+          <h2 className="text-5xl font-bold">{heading}</h2>
+          <p className="py-5 text-xl">{message}</p>
+        </div>
+
         {/* Wrap the button with Link */}
         <Link href="https://t.me/slavinskaofficiall">
           <a
