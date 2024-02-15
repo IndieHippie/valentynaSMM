cc3444063107a77027402ee7f141fcf9f120c3b9
IndieHippie <you@example.com>
2024-02-15 13:24:30 +0200

    commit

--- components/Slider.jsx
+++ components/Slider.jsx
@@ -19,7 +19,7 @@ const Slider = ({ slides }) => {
   }
 
   return (
-    <div id='gallery' className='max-w-[1240px] mx-auto mt-12'>
+    <div id='gallery' className='max-w-[1240px] mx-auto '>
       <h1 className='text-2xl font-bold text-center p-4'>Gallery</h1>
       <div className='relative flex justify-center p-4'>
 
